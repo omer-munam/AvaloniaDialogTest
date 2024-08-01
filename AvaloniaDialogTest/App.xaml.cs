891374a23a9ea965f9070e55ae709ffffb48ff20
Omer Munam <39843323+omer-munam@users.noreply.github.com>
2024-08-01 16:32:05 -0400

    Add project files.

---
+++ AvaloniaDialogTest/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AvaloniaDialogTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Testproj/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testproj
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            FileDialogTest fileDialogTest = new FileDialogTest();
+            fileDialogTest.OpenDialog();
+        }
+    }
+}
